3ac0d5b85b5c2f7e70ba75e64efadbc76423aa05
PrimeTimeTran <primetimetran@gmail.com>
2025-12-24 09:59:02 +0700

    Fix CORS issue

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,21 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:8081',
+        'http://127.0.0.1:3000',
+        'https://health-compiler-frontend-890407456021.us-east1.run.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
