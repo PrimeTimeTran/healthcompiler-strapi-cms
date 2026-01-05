94e4a07f4251bd810c848c2bac5112100df0cd4c
PrimeTimeTran <primetimetran@gmail.com>
2026-01-05 16:36:04 +0700

    Fix SSL for strapi

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,10 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
+
+  url: 'https://api.marketing.compile.health',
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
